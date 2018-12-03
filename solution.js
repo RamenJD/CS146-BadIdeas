8cae8942a199c832563c569f6cf1c403a1edab59
Jie Dai <43449626+RamenJD@users.noreply.github.com>
2018-12-03 18:06:46 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
+function liking() {
+document.getElementsByIdName("meme").addEventListener("mouseover", mouseover);
+document.getElementsByIdName("meme").addEventListener("mouseout", mouseout);
+}
 
+function mouseover(){
+	overlay.style.opacity = "1"
+}
+
+function mouseOut() {
+    overlay.style.opacity = "0.25";
+}
+
+
+window.onload = () => {
+   // Need to edit this;
+   meme.addEventListener('submit', liking);
+};
