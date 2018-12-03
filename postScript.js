da86d63f500f3a4d6f69e58fa177bdc79a29302e
Paul Gurman <43449472+Pgurman@users.noreply.github.com>
2018-12-03 17:23:04 -0500

    Add files via upload

---
+++ postScript.js
@@ -0,0 +1,35 @@
+function handleFormSubmit(event) {
+   event.preventDefault();
+
+   var imgLocation = './images/under-construction.jpg';
+   
+   addNewContent(imgLocation);
+}
+
+function validate(){
+	var x = document.getElementById("chooseImage").src;
+	alert(x);
+}
+
+function addNewContent(imgLocation) {
+	var Element = document.createElement("div");
+	Element.className = "content";
+	var Element2 = document.createElement("div");
+	Element2.className = "post";
+	var Image = document.createElement("img");
+	Image.className = "meme";
+	Image.src = imgLocation;
+	
+	
+	Element2.appendChild(Image);
+	Element.appendChild(Element2);
+	
+	document.getElementById("container").appendChild(Element);
+
+	
+}
+
+window.onload = () => {
+   // Once our window is loaded, we add the event listener for our post form
+   document.getElementById("Post").addEventListener('click', handleFormSubmit);
+};
